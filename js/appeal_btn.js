d5bd3ea95cd27ec686c87954b1df34dcfa141cef
Yuri Hanyu <hanyuu0509@gmail.com>
2022-09-05 23:15:53 +0900

    first commit

---
+++ js/appeal_btn.js
@@ -0,0 +1,18 @@
+'use strict';
+{
+
+    const appeal_btn = document.getElementById('appeal');
+    window.addEventListener("scroll", () => {
+    const currentY = window.pageYOffset;
+    if ( currentY < 3450){
+        setTimeout(function(){
+        appeal_btn.style.opacity = 1;
+        }, 1);
+    } else {
+        setTimeout(function(){
+        appeal_btn.style.opacity = 0;
+        }, 1);
+    }
+    });
+
+}
